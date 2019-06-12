a19eb3f0da6829393b651945d71223c989136816
JAYDA KADIR <jydkdr@gmail.com>
2019-06-12 15:07:53 +0100

    Add power saving mode - default true

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -7,21 +7,28 @@ describe ("Thermostat", function(){
 
   it("can create an instance of thermostat", function(){
     expect(thermostat instanceof Thermostat).toBe(true)
-  })
+  });
 
   it("it can have a start temperature", function(){
     expect(thermostat.startvalue).toEqual(20)
-  })
+  });
 
   it("it can raise temerature", function(){
     startval = thermostat.startvalue
     output = thermostat.tempUp(1)
     expect(output).toEqual(startval+1)
-  })
+  });
 
   it("it can decrease temperature", function(){
     startval = thermostat.startvalue
     output = thermostat.tempDown(1)
     expect(output).toEqual(startval-1)
-  })
-})
+  });
+
+  it("it check if power saving mode is on by default", function(){
+    thermostat.powerSavingMode()
+    expect(thermostat._powerSaving).toBe(true)
+  });
+
+
+});
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,6 +1,7 @@
 function Thermostat (startvalue = 20) {
   this.startvalue = startvalue;
   const minvalue = 10;
+  this._powerSaving = true;
 }
 
 Thermostat.prototype.tempUp = function(number){
@@ -9,4 +10,8 @@ Thermostat.prototype.tempUp = function(number){
 
 Thermostat.prototype.tempDown = function(number){
   return this.startvalue - number;
+}
+
+Thermostat.prototype.powerSavingMode = function(value=true){
+  return this._powerSaving = value;
 }
